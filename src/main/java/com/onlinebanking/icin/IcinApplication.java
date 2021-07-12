b3c82c91825260e5e033f7cc97f890c148a9aabb
Kasturi Chaudhari <kasturi.chaudhari11@gmail.com>
2021-07-11 20:14:47 -0700

    Create springboot project

---
+++ src/main/java/com/onlinebanking/icin/IcinApplication.java
@@ -0,0 +1,13 @@
+package com.onlinebanking.icin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlinebanking/icin/IcinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinebanking.icin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
